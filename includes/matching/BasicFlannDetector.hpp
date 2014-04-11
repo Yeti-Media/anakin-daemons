5bb6cf39e40d8386620a13d3ae26996098ab6803
Simon <sgutierrez@yeti-media.com>
2014-04-10 21:22:23 -0300

    added more comments

--- includes/matching/BasicFlannDetector.hpp
+++ includes/matching/BasicFlannDetector.hpp
@@ -11,12 +11,25 @@ namespace Anakin {
 /**
 * This class is used to detect patterns on a scene
 * Uses SerializableFlannBasedMatcher (SFBM) which is a FlannBasedMatcher with serialization capabilities
-*
 */
 class BasicFlannDetector {
     public:
+        /**
+        * Constructor
+        * detector  : a SFBM that will find patterns on a scene
+        * cache     : trainers, patterns and scenarios cache used by the SFBM
+        * minRatio  :   since the detection is done by getting two best matches for every descriptor
+        *               this sets the minimum value of the distance ratio between the the best match and the better one
+        * min_matches_allowed   :   the minimum matches to consider a pattern as found
+        */
         BasicFlannDetector( cv::Ptr<SerializableFlannBasedMatcher>  detector, SFBMCache* cache, float minRatio = 1.f / 1.5f, int min_matches_allowed = 8);
+        /**
+        * find patterns in a scene and returns a vector with every match obtained
+        */
         std::vector<Match>* findPatterns(RichImg* scene);
+        /**
+        * change the SFBM to use
+        */
         void changeMatcher(cv::Ptr<SerializableFlannBasedMatcher> matcher);
     protected:
         void getMatches(const cv::Mat& queryDescriptors, std::vector<cv::DMatch>& matches);
--- includes/matching/FlannMatchingProcessor.hpp
+++ includes/matching/FlannMatchingProcessor.hpp
@@ -9,9 +9,23 @@
 
 namespace Anakin {
 
+/**
+* This class is used to process a scene and find patterns,
+* the results from the matching process are translated into a more
+* friendly format (see ResultWriter)
+*
+*/
 class FlannMatchingProcessor {
     public:
+        /**
+        * Constructor
+        * detector  : the detector to be used in order to find matches
+        * rw        : a ResultWriter used to translate the results obtained from the detector
+        */
         FlannMatchingProcessor(BasicFlannDetector* detector, ResultWriter* rw);
+        /**
+        *   Will run the detector with the scene and then translate the results
+        */
         std::vector<JSONValue*>* process(RichImg* scene);
     protected:
     private:
